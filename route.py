93662de98a830cbaf0ee6f6a46c974b4a633cffd
igor-ribeiiro <ribeiro_igor@hotmail.com>
2018-06-23 18:03:06 -0300

    Changing name of frontEnd to public

--- route.py
+++ route.py
@@ -1,63 +1,31 @@
-import tornado.web, tornado.ioloop
-import motor
+import os
+import tornado.ioloop
+import tornado.web as web
+
+public_root = os.path.join(os.path.dirname(__file__), 'public')
 
 
-class NewMessageHandler(tornado.web.RequestHandler):
+class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        """Show a 'compose message' form"""
-        self.write('''
-        <form method="post">
-            <input type="text" name="msg">
-            <input type="submit">
-        </form>''')
-
-    # Method exits before the HTTP request completes, thus "asynchronous"
-    @tornado.web.asynchronous
-    def post(self):
-        """Insert a message
-        """
-        msg = self.get_argument('msg')
-
-        # Async insert; callback is executed when insert completes
-        self.settings['db'].messages.insert(
-            {'msg': msg},
-            callback=self._on_response)
-
-    def _on_response(self, result, error):
-        if error:
-            raise tornado.web.HTTPError(500, error)
-        else:
-            self.redirect('/')
-
-class MessagesHandler(tornado.web.RequestHandler):
-    @tornado.web.asynchronous
-    def get(self):
-        """Display all messages
-        """
-        self.write('<a href="/compose">Compose a message</a><br>')
-        self.write('<ul>')
-        db = self.settings['db']
-        db.messages.find().sort([('_id', -1)]).each(self._got_message)
-
-    def _got_message(self, message, error):
-        if error:
-            raise tornado.web.HTTPError(500, error)
-        elif message:
-            self.write('<li>%s</li>' % message['msg'])
-        else:
-            # Iteration complete
-            self.write('</ul>')
-            self.finish()
+        self.render('index.html')
 
 
-db = motor.MotorConnection().open_sync().test
+handlers = [
+  (r'/public/(.*)', web.StaticFileHandler, {'path': public_root}),
+  (r'/', MainHandler)
+]
 
-application = tornado.web.Application([
-        (r'/compose', NewMessageHandler),
-        (r'/', MessagesHandler)
-    ], db=db
+settings = dict(
+  debug=True,
+  static_path=public_root,
+  template_path=public_root
 )
 
-print ('Listening on http://localhost:8888')
-application.listen(8888)
-tornado.ioloop.IOLoop.instance().start()
+application = web.Application(handlers, **settings)
+
+if __name__ == "__main__":
+    port = int(8888)
+    application.listen(port)
+    str(port)
+    print("Server running on port " + str(port))
+    tornado.ioloop.IOLoop.instance().start()
